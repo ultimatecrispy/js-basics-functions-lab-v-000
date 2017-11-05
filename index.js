b0677672288ed7d127582d68dd3e4ba2661d9e34
Tom Su <ultimatecrispy@gmail.com>
2017-11-05 17:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function distanceTravelledInFeet(start, finish) {
 function calculatesFarePrice(start, finish) {
   distance = finish - start;
   switch (distance) {
-    case (distance <= 400)
+    case (distance <= 400):
       return "Free";
       break;
     default:
